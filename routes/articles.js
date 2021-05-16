1bf4335d09675d00900c6df96a3d13b115315514
undeadgorilla <dennis.marx93@gmail.com>
2021-05-16 16:30:17 +0200

    version 0.1

---
+++ routes/articles.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const Article = require("./../models/article");
+const router = express.Router();
+
+router.get("/new", (req, res) => {
+  res.render("articles/new")
+})
+
+router.post("/", (req, res) => {
+  const article = new Article ({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  })
+})
+
+module.exports = router;
